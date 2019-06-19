d1829452bd9f018195f074e6a2df4c1da96c7981
Michael <mhaviv18@gmail.com>
2019-06-19 08:28:58 -0400

    First commit with Meme Me 1.0 version

---
+++ ViewController.swift
@@ -0,0 +1,177 @@
+//
+//  ViewController.swift
+//  Meme Me
+//
+//  Created by Michael Haviv on 6/17/19.
+//  Copyright © 2019 Michael Haviv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate, UITextFieldDelegate {
+    
+    @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var photosButton: UIBarButtonItem!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topTextFieldContainerView: UIView!
+    @IBOutlet weak var bottomTextFieldContainerView: UIView!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    let imagePicker = UIImagePickerController()
+    let labelText = "Caption".uppercased()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        topTextField.delegate = self
+        bottomTextField.delegate = self
+        
+        textFieldAttributes(textField: topTextField)
+        textFieldAttributes(textField: bottomTextField)
+                
+    }
+    
+    func textFieldAttributes(textField: UITextField) {
+        topTextField.translatesAutoresizingMaskIntoConstraints = false
+        bottomTextField.translatesAutoresizingMaskIntoConstraints = false
+        topTextField.text = labelText
+        bottomTextField.text = labelText
+        topTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        topTextField.textAlignment = .center
+        bottomTextField.textAlignment = .center
+        topTextField.backgroundColor = UIColor.clear
+        bottomTextField.backgroundColor = UIColor.clear
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == labelText {
+            textField.text = nil
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField.text!.isEmpty {
+            textField.text = labelText
+        }
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        print("subscribe")
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        print("unsubscribe!")
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object:  nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func textFieldTapRecognizer() {
+        var topTextFieldTap: UITapGestureRecognizer = UITapGestureRecognizer(target: topTextField, action: #selector(UITextFieldDelegate.textFieldDidBeginEditing(_:)))
+        var bottomTextFieldTap: UITapGestureRecognizer = UITapGestureRecognizer(target: bottomTextField, action: #selector(UITextFieldDelegate.textFieldDidBeginEditing(_:)))
+        
+        topTextFieldTap.cancelsTouchesInView = false
+        bottomTextFieldTap.cancelsTouchesInView = false
+        
+        if topTextField.isFirstResponder {
+            view.addGestureRecognizer(topTextFieldTap)
+        }
+        if bottomTextField.isFirstResponder{
+            view.addGestureRecognizer(bottomTextFieldTap)
+        }
+        if view.isFirstResponder {
+            topTextFieldTap = UITapGestureRecognizer(target: topTextField, action: #selector(UITextFieldDelegate.textFieldDidEndEditing(_:)))
+            bottomTextFieldTap = UITapGestureRecognizer(target: bottomTextField, action: #selector(UITextFieldDelegate.textFieldDidBeginEditing(_:)))
+        }
+    }
+    
+    @objc func keyboardWillShow(_ notification:Notification) {
+        print("Keyboard Show!")
+        textFieldTapRecognizer()
+        if bottomTextField.isFirstResponder {
+            self.view.frame.origin.y -= getKeyboardHeight(notification)
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification:Notification) {
+        print("Keyboard Hide!")
+        view.becomeFirstResponder()
+        textFieldTapRecognizer()
+        self.view.frame.origin.y = 0
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    let memeTextAttributes: [NSAttributedString.Key: Any] = [
+        //        NSAttributedString.Key.strokeColor : UIColor.black,
+        
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+        NSAttributedString.Key.font: UIFont(name: "impact", size: 28)!,
+        //        NSAttributedString.Key.strokeWidth:  3.0
+    ]
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) || UIImagePickerController.isSourceTypeAvailable(.camera) {
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                imagePickerView.contentMode = .scaleAspectFit
+                imagePickerView.image = image
+                dismiss(animated: true, completion: nil)
+            } else {
+                dismiss(animated: true, completion: nil)
+            }
+        }
+    }
+    
+    @IBAction func pickAnImage(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.mediaTypes = ["public.image", "public.movie"]
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func pickAnImageFromCamera(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        imagePicker.mediaTypes = ["public.image", "public.movie"]
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+
+}
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
