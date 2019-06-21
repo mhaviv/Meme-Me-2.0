bd09254595d2d818401bdf07230ab42337c1be74
Michael <mhaviv18@gmail.com>
2019-06-21 09:21:35 -0400

    added Meme struct

---
+++ Meme.swift
@@ -0,0 +1,16 @@
+//
+//  Meme.swift
+//  Meme Me
+//
+//  Created by Michael Haviv on 6/20/19.
+//  Copyright © 2019 Michael Haviv. All rights reserved.
+//
+
+import UIKit
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memedImage: UIImage
+}
