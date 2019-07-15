df83df1953d5c2f9d889eb72861e901510691a99
Michael <mhaviv18@gmail.com>
2019-07-15 14:19:32 -0400

    added collection view controller functionality and flow layout styling, added empty view functionality, added reload data funcitonality on viewDidAppear

---
+++ SentMemesCollectionViewController.swift
@@ -0,0 +1,72 @@
+//
+//  SentMemesCollectionViewController.swift
+//  Meme Me 2.0
+//
+//  Created by Michael Haviv on 7/8/19.
+//  Copyright © 2019 Michael Haviv. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+private let reuseIdentifier = "SentMemesCollectionViewCell"
+
+class SentMemesCollectionViewController: UICollectionViewController {
+    
+    var memes: [Meme]! {
+        let object = UIApplication.shared.delegate
+        let appDelegate = object as! AppDelegate
+        return appDelegate.memes
+    }
+    
+//    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let space:CGFloat = 3.0
+        let dimension = (view.frame.size.width - (2 * space)) / 3.0
+        
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.itemSize = CGSize(width: dimension, height: dimension)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        collectionView?.reloadData()
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        if collectionView != nil {
+            collectionView?.reloadData()
+        }
+    }
+    
+    func showEmptyView(_ show: Bool) {
+        if show {
+            let label: UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: collectionView!.frame.width, height: collectionView!.frame.height))
+            label.numberOfLines = 2
+            label.textAlignment = .center
+            label.text = "No Memes Stored!\nClick '+' to create a new Meme."
+            collectionView!.backgroundView = label
+        } else {
+            collectionView!.backgroundView = nil
+        }
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        memes.count == 0 ? showEmptyView(true) : showEmptyView(false)
+        return memes.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SentMemesCollectionViewCell", for: indexPath) as! SentMemesCollectionViewCell
+        let meme = memes[indexPath.row]
+        cell.collectionImageView.image = meme.memedImage
+        return cell
+    }
+}
