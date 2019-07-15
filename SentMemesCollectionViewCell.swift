b485e1e813caff8ec14fd98e99e3fefea3c7e747
Michael <mhaviv18@gmail.com>
2019-07-15 14:17:24 -0400

    added the table and collection view cell files

--- SentMemesCollectionViewCell.swift
+++ SentMemesCollectionViewCell.swift
@@ -9,38 +9,8 @@
 import Foundation
 import UIKit
 
-class MemeCollectionViewCell: UIViewController {
+class SentMemesCollectionViewCell: UICollectionViewCell {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet weak var collectionImageView: UIImageView!
     
-
-//    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MemeCollectionViewCell", for: indexPath) as! MemeCollectionViewCell
-//        let villain = self.allVillains[(indexPath as NSIndexPath).row]
-//        
-//        // Set the name and image
-//        cell.nameLabel.text = Meme.name
-//        cell.imageView?.image = UIImage(named: Meme.imageName)
-//        
-//        return cell
-//    }
-//    
-//    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath:IndexPath) {
-//        
-//        // Grab the DetailVC from Storyboard
-//        let detailController = self.storyboard!.instantiateViewController(withIdentifier: "VillainDetailViewController") as! VillainDetailViewController
-//        
-//        //Populate view controller with data from the selected item
-//        detailController.villain = allVillains[(indexPath as NSIndexPath).row]
-//        
-//        // Present the view controller using navigation
-//        navigationController!.pushViewController(detailController, animated: true)
-//        
-//    }
-
 }
---
+++ SentMemesTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SentMemesTableViewCell.swift
+//  Meme Me 2.0
+//
+//  Created by Michael Haviv on 7/14/19.
+//  Copyright © 2019 Michael Haviv. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SentMemesTableViewCell: UITableViewCell {
+
+    @IBOutlet var cellImageView: UIImageView!
+    @IBOutlet var cellTopTextLabel: UILabel!
+    @IBOutlet var cellBottomTextLabel: UILabel!
+
+}
