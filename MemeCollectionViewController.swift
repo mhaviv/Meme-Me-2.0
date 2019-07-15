b64b0bc462b3c0be135c5363da161733c019309c
Michael <mhaviv18@gmail.com>
2019-07-15 14:25:08 -0400

    renamed MemeCollectionViewController to SentMemesCollectionViewController, added tab bar view controller, added navigation controllers for the table and collection views, added the table and collection views, added the add nav button to connect to meme controller, added table and collection view tab bar icons

--- MemeCollectionViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MemeCollectionViewController.swift
-//  Meme Me 2.0
-//
-//  Created by Michael Haviv on 7/8/19.
-//  Copyright © 2019 Michael Haviv. All rights reserved.
-//
-
-import Foundation
