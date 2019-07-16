91a72d8d8ce44aacb853049562f914502576db67
Michael <mhaviv18@gmail.com>
2019-07-15 22:42:12 -0400

    added logic to pull selected meme object, hides/shows tab bar when detail view appears or disappears

--- MemeDetailViewController.swift
+++ MemeDetailViewController.swift
@@ -10,21 +10,19 @@ import UIKit
 
 class MemeDetailViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var memeImage: UIImageView!
+    var meme: Meme!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        memeImage.image = meme.memedImage
+        self.tabBarController?.tabBar.isHidden = true
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        self.tabBarController?.tabBar.isHidden = false
     }
-    */
-
+    
 }
